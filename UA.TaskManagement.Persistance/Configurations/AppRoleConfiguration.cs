1d96af31c06118bf4f029c2fd71edb8c7b230a3d
Umut Atraş <umutatras@outlook.com>
2024-07-29 13:09:43 +0300

    configuration v2

---
+++ UA.TaskManagement.Persistance/Configurations/AppRoleConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using UA.TaskManagement.Domain.Entities;
+
+namespace UA.TaskManagement.Persistance.Configurations;
+
+public class AppRoleConfiguration : IEntityTypeConfiguration<AppRole>
+{
+    public void Configure(EntityTypeBuilder<AppRole> builder)
+    {
+        builder.Property(x=>x.Definition).IsRequired();
+        builder.Property(x => x.Definition).HasMaxLength(500);
+        builder.HasMany(x=>x.Users).WithOne(x=>x.Role).HasForeignKey(x=>x.AppRoleId);
+    }
+}
---
+++ UA.TaskManagement.Persistance/Configurations/AppUserConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using UA.TaskManagement.Domain.Entities;
+
+namespace UA.TaskManagement.Persistance.Configurations
+{
+    public class AppUserConfiguration : IEntityTypeConfiguration<AppUser>
+    {
+        public void Configure(EntityTypeBuilder<AppUser> builder)
+        {
+
+        }
+    }
+}
