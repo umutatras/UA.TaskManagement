ac9fd7edf1622e15fc85a2bdab0027ad78985ee3
Umut Atraş <umutatras@outlook.com>
2024-07-29 12:43:43 +0300

    context oluşturuldu

---
+++ UA.TaskManagement.Persistance/Context/TaskManagementContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UA.TaskManagement.Domain.Entities;
+
+namespace UA.TaskManagement.Persistance.Context;
+
+public class TaskManagementContext : DbContext
+{
+    public TaskManagementContext(DbContextOptions<TaskManagementContext> options) : base(options)
+    {
+    }
+
+    public DbSet<AppRole> Roles { get; set; }
+    public DbSet<AppTask> Tasks { get; set; }
+    public DbSet<Priority> Priorities { get; set; }
+    public DbSet<TaskReport> TaskReports { get; set; }
+    public DbSet<AppUser> Users { get; set; }
+    public DbSet<Notification> Notifications { get; set; }
+}
