a4bc8e91b0909b67ae4bffb5dccfca75cf69fe4a
Umut Atraş <umutatras@outlook.com>
2024-07-30 14:29:07 +0300

    register tasarımı

---
+++ UA.TaskManagement.Application/DTOs/AccountDto.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UA.TaskManagement.Application.DTOs
+{
+    public record LoginDto(string Name,string Password);
+}
--- UA.TaskManagement.UI/Controllers/AccountController.cs
+++ UA.TaskManagement.UI/Controllers/AccountController.cs
@@ -1,10 +1,16 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using UA.TaskManagement.Application.DTOs;
 
 namespace UA.TaskManagement.UI.Controllers
 {
     public class AccountController : Controller
     {
         public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Login(LoginDto dto)
         {
             return View();
         }
