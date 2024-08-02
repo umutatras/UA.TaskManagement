20e844775f2d54248f571c2fc6a59117791916df
Umut Atraş <umutatras@outlook.com>
2024-08-02 12:01:33 +0300

    priorty req dtos

---
+++ UA.TaskManagement.Application/DTOs/PriorityDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UA.TaskManagement.Application.DTOs
+{
+    public record PriorityListDto(int Id, string Definition);
+
+}
---
+++ UA.TaskManagement.Application/Request/PriorityRequest.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UA.TaskManagement.Application.DTOs;
+
+namespace UA.TaskManagement.Application.Request
+{
+    public record PriortyListRequest():IRequest<Result<List<PriorityListDto>>>;
+    public record PriortyGetByIdRequest(int id):IRequest<Result<PriorityListDto>>;
+    public record PriortyDeleteRequest(int id):IRequest<Result<NoData>>;
+    public record PriortyUpdateRequest(int id,string Definition):IRequest<Result<NoData>>;
+    public record PriortyCreateRequest(string Definition):IRequest<Result<NoData>>;
+
+}
