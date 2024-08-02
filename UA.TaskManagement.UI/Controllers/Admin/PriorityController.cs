916bb321fa29b7cb79949526cd0246197efcd189
Umut Atraş <umutatras@outlook.com>
2024-08-02 14:58:34 +0300

    update delete endpoint yazıldı

--- UA.TaskManagement.UI/Controllers/Admin/PriorityController.cs
+++ UA.TaskManagement.UI/Controllers/Admin/PriorityController.cs
@@ -54,5 +54,57 @@ namespace UA.TaskManagement.UI.Controllers.Admin
             }
 
         }
+        public async Task<IActionResult> Delete(int id)
+        {
+            ViewBag.Active = "Priority";
+            await this.mediator.Send(new PriortyDeleteRequest(id));
+            return RedirectToAction("List");
+
+        }
+
+        public async Task<IActionResult> Update(int id)
+        {
+            ViewBag.Active = "Priority";
+            var result = await this.mediator.Send(new PriortyGetByIdRequest(id));
+            if (result.IsSuccess)
+            {
+                var requestModel = new PriortyUpdateRequest(result.Data.Id, result.Data.Definition);
+                return View(requestModel);
+            }
+            else
+            {
+                ModelState.AddModelError("", result.ErrorMessage ?? "Bilinmeyen bir hata oluştu, sistem üreticinize başvurun");
+                var requestModel = new PriortyUpdateRequest(0, null);
+                return View(requestModel);
+            }
+
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Update(PriortyUpdateRequest request)
+        {
+            ViewBag.Active = "Priority";
+            var result = await this.mediator.Send(request);
+            if (result.IsSuccess)
+            {
+                return RedirectToAction("List");
+            }
+            else
+            {
+                if (result.Errors?.Count > 0)
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError(error.propertyName, error.ErrorMessage);
+                    }
+                }
+                else
+                {
+                    ModelState.AddModelError("", result.ErrorMessage ?? "Bilinmeyen bir hata oluştu, sistem üreticinize başvurun");
+                }
+
+                return View(request);
+            }
+        }
     }
 }
