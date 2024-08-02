44a4a2d2abd7506c5b45f7ca7ec282934faa60a9
Umut Atraş <umutatras@outlook.com>
2024-08-02 11:31:25 +0300

    Priority ekranı ayarlaması

---
+++ UA.TaskManagement.UI/Controllers/Admin/PriorityController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UA.TaskManagement.UI.Controllers.Admin
+{
+    public class PriorityController : Controller
+    {
+        [Area("Admin")]
+        [Authorize(Roles = "Admin")]
+        public IActionResult List()
+        {
+            return View();
+        }
+    }
+}
