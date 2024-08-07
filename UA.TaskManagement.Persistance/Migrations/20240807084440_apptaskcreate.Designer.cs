c03994af8c2ba1555f3be6e0fb288184a82aec5f
Umut Atraş <umutatras@outlook.com>
2024-08-07 11:45:24 +0300

    migration

--- UA.TaskManagement.Persistance/Migrations/20240807084440_apptaskcreate.Designer.cs
+++ UA.TaskManagement.Persistance/Migrations/20240807084440_apptaskcreate.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using UA.TaskManagement.Persistance.Context;
 
@@ -11,9 +12,11 @@ using UA.TaskManagement.Persistance.Context;
 namespace UA.TaskManagement.Persistance.Migrations
 {
     [DbContext(typeof(TaskManagementContext))]
-    partial class TaskManagementContextModelSnapshot : ModelSnapshot
+    [Migration("20240807084440_apptaskcreate")]
+    partial class apptaskcreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ UA.TaskManagement.Persistance/Migrations/20240807084440_apptaskcreate.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UA.TaskManagement.Persistance.Migrations
+{
+    /// <inheritdoc />
+    public partial class apptaskcreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Tasks_Users_AppUserId",
+                table: "Tasks");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "AppUserId",
+                table: "Tasks",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Tasks_Users_AppUserId",
+                table: "Tasks",
+                column: "AppUserId",
+                principalTable: "Users",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Tasks_Users_AppUserId",
+                table: "Tasks");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "AppUserId",
+                table: "Tasks",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Tasks_Users_AppUserId",
+                table: "Tasks",
+                column: "AppUserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
